6027fe809ad745d77300bfe799e61bfe39cfe170
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-01-04 21:59:07 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,19 +13,24 @@ function main() {
                 timer.innerHTML = createTimerBySeconds(seconds);
             }, 1000);
             timer.classList.remove('timer-paused');
+            changeButton();
         }
+
         if (element.classList.contains('pause')) {
             if (timerStart !== undefined) {
                 clearInterval(timerStart);
                 timer.classList.add('timer-paused');
             }
+            changeButton();
         }
+
         if (element.classList.contains('reset')) {
             clearInterval(timerStart);
             seconds = 0;
             timerStart = undefined;
             timer.innerHTML = createTimerBySeconds(seconds);
             timer.classList.remove('timer-paused');
+            changeButton();
         }
     });
 
@@ -35,6 +40,20 @@ function main() {
             hour12: false,
             timeZone: 'GMT'
         });
+    }
+
+    function changeButton() {
+        const startButton = document.querySelector('.start');
+        const pauseButton = document.querySelector('.pause');
+
+        if (timerStart === undefined) {
+            startButton.classList.add('active');
+            pauseButton.classList.remove('active');
+            return;
+        }
+
+        startButton.classList.toggle('active');
+        pauseButton.classList.toggle('active');
     }
 }
 
